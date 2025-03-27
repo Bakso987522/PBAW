e67fbcaf173812afe4f523ff7f82d8d878966303
Jakub Bakaj <jakub.bakajj@gmail.com>
2025-03-27 16:21:26 +0100

    Zmiana konfiguracji

--- zadanie4/config.php
+++ zadanie4/config.php
@@ -4,8 +4,8 @@ require_once 'Config.class.php';
 $conf = new Config();
 
 $conf->root_path = dirname(__FILE__);
-$conf->server_name = 'localhost';
-$conf->server_url = 'http://'.$conf->server_name;
+$conf->server_name = $_SERVER['HTTP_HOST'];
+$conf->server_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . '://' . $conf->server_name;
 $conf->app_root = '/zadanie4';
 $conf->app_url = $conf->server_url.$conf->app_root;
 $conf->action_root = $conf->app_root.'/app/controller.php?action=';
